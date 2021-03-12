d0dbb18fe70ddf232db7486f54147c9fba42fc20
wildrider09 <77229401+wildrider09@users.noreply.github.com>
2021-03-12 21:36:05 +0530

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,50 +14,49 @@ public class Calculator {
 
         while(check){
             try{
-                System.out.println("Press 1:Square Root, 2:Factorial, 3:Natural Logarithm, 4:Power Funtion, 5:Exit");
+                System.out.println("Press 1:Square Root,\t 2:Factorial,\t 3:Natural Logarithm,\t 4:Power Funtion,\t 5:Exit");
                 System.out.print("Enter your choice:");
                 choice=sc.nextInt();
                 switch (choice){
                     case 1:
-                        System.out.print("Enter a positive number:");
+                        System.out.print("Enter a number for calculating square-root::");
                         a= sc.nextDouble();
                         c=sqrt(a);
                         if(c<0)
-                            System.out.println("Number can not be negetive");
+                            System.out.println("Square root of negative number can not be calculated");
                         else
-                            System.out.println("Answer is: "+c);
+                            System.out.println("The resultant output is: "+c);
                         break;
-                    case 3: System.out.print("Enter a number greater than 0:");
+                    case 3: System.out.print("Enter a number for calculating natural-logarithm::");
                         double a= sc.nextDouble();
                         c=log(a);
                         if(c<0)
-                             System.out.println("Number can not be less than or equal to zero");
+                             System.out.println("Input number can not be less than or equal to zero for finding log");
                         else
-                             System.out.println("Answer is: "+ c);
+                             System.out.println("The resultant output is: "+ c);
                         break;
                     case 4:
-                        System.out.print("Enter two numbers with space (a^b: a b):");
+                        System.out.print("Enter two numbers separated by space for calculating power(a^b: a b)::");
                         a= sc.nextDouble();
                         b= sc.nextDouble();
                         c=pow(a,b);
-                        System.out.println("Answer is: "+ c);
+                        System.out.println("The resultant output is: "+ c);
                         break;
                     case 2:
-                        System.out.print("Enter a positive number:");
+                        System.out.print("Enter a number for calculating factorial::");
                         int f=sc.nextInt();
-                        BigInteger fac= fact(f);
-                        int test= fac.compareTo(new BigInteger("0"));
-                        if(test<0)
-                            System.out.println("Number can not be negetive");
+                        long fac= fact(f);
+                        if(fac<0)
+                            System.out.println("Factorial of negative number can not be calculated");
                         else
                             System.out.println("Answer is: "+ fac);
                         break;
                     case 5:
-                        System.out.println("Thank you for using Calculator");
+                        System.out.println("Thank you for using calculator;Have a nice day!!!!");
                         check=false;
                         break;
                     default:
-                        System.out.println("Wrong Choice,enter a valid number");
+                        System.out.println("X Wrong choice entered,please enter a valid number X");
                 }
             }
             catch (Exception inp){
@@ -72,12 +71,12 @@ public class Calculator {
         c=Math.sqrt(a);
         return c;
     }
-    public static BigInteger fact(int f){
+    public static long fact(int f){
         if(f<0){
-            return new BigInteger("-1");
+            return -1;
         }
-        BigInteger ans = BigIntegerMath.factorial(f);
-        return ans;
+        if(f<2) return 1;
+        return f*fact(f-1);
     }
     public static double log(double a){
         if(a<=0){
